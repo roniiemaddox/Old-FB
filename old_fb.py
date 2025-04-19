c89dd169c5b94de2ac77fd0e5fdc507908360511
Rizwan Ali <120648247+Rizwanali444@users.noreply.github.com>
2025-04-19 08:56:51 +0500

    Update old_fb.py

--- old_fb.py
+++ old_fb.py
@@ -1,2 +1,14 @@
-import coconuts
+import traceback
+
+print("Script Starting... Please wait!")
+
+try:
+    import coconuts
+    print("Coconuts module loaded successfully.")
+except Exception as e:
+    print("Error importing coconuts:", e)
+    traceback.print_exc()
+
+# Baaki code ab yahan se likho
+
 
